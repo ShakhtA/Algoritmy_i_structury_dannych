e35cfe524a1c60217aa50eb7900e1b594325a322
Andrey <shakht_a@mail.ru>
2023-05-31 22:23:55 +0300

    add reverse list

---
+++ ReverseDoubleList/ReverseDoubleList.cs
@@ -0,0 +1,31 @@
+﻿// Разворот двухсвязного списка
+
+public class List
+{
+    public class Node
+    {
+        Node head;
+        Node tail;
+
+        public void reverse()
+        {
+            Node currentNode = head;
+            while (currentNode != null)
+            {
+                next = currentNode.next;
+                prev = currentNode.prev;
+                currentNode.next = prev;
+                currentNode.prev = next;
+                if (prev == null)
+                    tail = currentNode;
+                if (next == null)
+                    head == currentNode;
+                currentNode = currentNode.next;
+            }
+        }
+
+        int value;
+        Node next;
+        Node prev;
+    }
+}
